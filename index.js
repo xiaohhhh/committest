39b79cade46bc97ac09fb0ab0d40e9a7d3c5299f
harry <346026240@qq.com>
2019-04-11 17:32:28 +0800

    update some text

---
+++ index.js
@@ -0,0 +1,28 @@
+const fs = require('fs')
+const shell = require('shelljs');
+
+const sleep = function(sec) {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            res();
+        }, sec * 1000)
+    })
+};
+
+(async () => {
+
+    let i=0;
+    for (i=0;i<10000;i++) {
+        try {
+        
+            fs.appendFileSync('./aa', 'addText')
+            
+            shell.exec(`git add ./`);
+            // git commit --date="10 day ago" -m "Your commit message" 
+            shell.exec(`git commit --date="1000 day ago" -m "update some text"`);
+
+        } catch (error) {
+            console.error('error', error);
+        }
+    }
+})();
